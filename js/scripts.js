745a967ea905e95aa5be017a8eb9bfe536d7b093
Nick ACCD <nickpetrilla@gmail.com>
2019-11-14 11:05:32 -0800

    Added images to html + jquery to hide and show divs depending on radiobutton

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 jQuery(function() {
 
-$("#birth-result-side").hide();
-$("#death-result-side").hide();
+$('#birth-result-side').hide();
+$('#death-result-side').hide();
 
-$("#submit-button").click(function(){
-	$("#default-result-side").hide();
-	$("#birth-result-side").show();
+
+$('#submit-button').click(function(){
+	if ($('#inlineRadioBirth').is(':checked')){
+		$('#birth-result-side').show();
+		$('#default-result-side').hide();
+		$('#death-result-side').hide();
+
+	} else if ($('#inlineRadioDeath').is(':checked')){
+
+		$('#death-result-side').show();
+		$('#birth-result-side').hide();
+		$('#default-result-side').hide();
+		$('#coffin').fadeIn();
+	}
 });
 
 });
