7605dd8a8164677f1cbd64d2ec7903aaa7c49b13
Nick ACCD <nickpetrilla@gmail.com>
2019-11-13 16:17:27 -0800

    Html + css + limited jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 jQuery(function() {
 
-	$('#my_button').click(function () {
-	});
+$("#birth-result-side").hide();
+$("#death-result-side").hide();
+
+$("#submit-button").click(function(){
+	$("#default-result-side").hide();
+	$("#birth-result-side").show();
+});
 
 });
